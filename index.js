98038f91cf493db048668cda37d6163038d3b18b
garrettBourque <bourque.garrett@gmail.com>
2021-05-17 15:39:45 -0500

    Completed assignemtn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value){
+
+    return Math.abs(value-42);
+}
+
+function distanceFromHqInFeet(value){
+    return distanceFromHqInBlocks(value)*264;
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start-end)*264;
+}
+
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start,end);
+
+    if(distance>2500)
+        return 'cannot travel that far';
+    else if(distance<400)
+        return 0;
+    else if(distance>2000)
+        return 25;
+    else 
+        return (distance-400)*.02;
+}
